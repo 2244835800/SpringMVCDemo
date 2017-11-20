a7ede7d150009a3f6e687c16cc52152bfec4c954
hepo <hepo@DESKTOP-GO6B6K0>
2017-11-21 03:29:53 +0800

    noting

---
+++ src/com/springmvc/controllers/HomeController.java
@@ -0,0 +1,29 @@
+package com.springmvc.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+	public HomeController() {
+		// TODO Auto-generated constructor stub
+	}
+	@RequestMapping(value="/home")
+	public String index(){
+		return "index";
+		
+	}
+	
+	@RequestMapping(value="/home2")
+	public String index2(){
+		return "index2";
+		
+	}
+	
+	@RequestMapping(value="/")
+	public String defaultIndex(){
+		return "index3";
+	}
+	
+}
